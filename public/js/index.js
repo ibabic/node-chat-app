d4ac596b424af43849b948da1d5fb11fb7859a9f
Ivan Babic <ibabic@fesb.com>
2018-12-04 12:11:43 +0100

    Add form for messages and show incoming messages

--- public/js/index.js
+++ public/js/index.js
@@ -11,4 +11,20 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (msg) {
     console.log('New message', msg);
+    var li = jQuery('<li></li>');
+    li.text(`${msg.from}: ${msg.text}`);
+
+    jQuery('#messages').append(li);
 });
+
+
+jQuery('#message-form').on('submit', (e) => {
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'User',
+        text: jQuery('[name=message]').val()
+    }, function () {
+
+    });
+})
